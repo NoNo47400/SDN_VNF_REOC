2021dc6ff80c117bfb22721fb3ea04850d6c3c99
Raspeur <101175169+Raspeur@users.noreply.github.com>
2024-12-07 09:54:09 +0100

    Ajout des adresses IP au "run" des container
    Mtn au lieu de seulement build les conteneurs, le fichier python les lance et leur attribue des adresses IP.

--- docker_test/create_dockers.py
+++ docker_test/create_dockers.py
@@ -1,24 +1,29 @@
 import os
 import subprocess
 
-# List of Dockerfiles and corresponding image names
+# List of Dockerfiles and corresponding image names and IP addresses
 services = {
-    'app1': 'application.Dockerfile',
-    'app2': 'application.Dockerfile',
-    'app3': 'application.Dockerfile',
-    'server': 'server.Dockerfile',
-    'gateway_intermediaire': 'gateway_intermediate.Dockerfile',
-    'gateway_finale1': 'gateway_final.Dockerfile'
+    'app1': ('application.Dockerfile', '172.18.0.2'),
+    'app2': ('application.Dockerfile', '172.18.0.3'),
+    'app3': ('application.Dockerfile', '172.18.0.4'),
+    'server': ('server.Dockerfile', '172.18.0.5'),
+    'gateway_intermediaire': ('gateway_intermediate.Dockerfile', '172.18.0.6'),
+    'gateway_finale1': ('gateway_final.Dockerfile', '172.18.0.7')
 }
 
 # Base directory for Dockerfiles
 base_dir = './'
 
-# Function to build Docker images
-def build_images():
-    for service, dockerfile in services.items():
+# Create a custom Docker network
+network_name = 'custom_net'
+subprocess.run(['docker', 'network', 'create', '--subnet=172.18.0.0/16', network_name], check=True)
+
+# Function to build Docker images and run containers
+def build_and_run_images():
+    for service, (dockerfile, ip_address) in services.items():
         dockerfile_path = os.path.join(base_dir, dockerfile)
         image_name = f'{service}-image'
+        
         # Determine the build argument name based on the service
         build_arg_name = ''
         if 'app' in service:
@@ -32,6 +37,7 @@ def build_images():
         elif 'server' in service:
             build_arg_name = 'SRV_NAME'
 
+        # Build the Docker image
         print(f'Building {image_name} from {dockerfile_path} with {build_arg_name}={service}')
         subprocess.run([
             'docker', 'build', '-t', image_name,
@@ -39,5 +45,14 @@ def build_images():
             '-f', dockerfile_path, base_dir
         ], check=True)
 
+        # Run the Docker container with a specific IP address
+        container_name = f'{service}-container'
+        print(f'Running {container_name} with IP {ip_address}')
+        subprocess.run([
+            'docker', 'run', '-d', '--name', container_name,
+            '--network', network_name, '--ip', ip_address,
+            image_name
+        ], check=True)
+
 if __name__ == "__main__":
-    build_images()
+    build_and_run_images()
